111b5deed0fa1e93fa83557844754a269ed25a80
Rewind <sploing1@gmail.com>
2018-07-09 00:28:26 -0700

    Input stuff works and started on action conditionals

--- Platform Fighter/Assets/_SCRIPTS/PLAYER/PlayerMovement.cs
+++ Platform Fighter/Assets/_SCRIPTS/PLAYER/PlayerMovement.cs
@@ -1,9 +1,18 @@
+using Rewired.UI.ControlMapper;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerInput input;
+
+    // TEMP VARIABLES WE ARE NOT USING THESE
+
+    // (false - left, true - right)
+    private bool facing;
+
+    private bool grounded;
+
 
     private void Start()
     {
@@ -12,6 +21,81 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        Types.ActionType action;
+
+        if (facing)
+        {
+            if (grounded)
+            {
+                
+                if (input.strongRight)
+                {
+                    if (input.neutral)
+                    {
+                        action = Types.ActionType.DASHATK;
+                    }
+                }
+                else if (input.lightRight)
+                {
+                    if (input.neutral)
+                    {
+                        action = Types.ActionType.FTILT;
+                    }
+                }
+                else if (input.lightRight || input.strongRight)
+                {
+                    if (input.special)
+                    {
+                        action = Types.ActionType.FSPECIAL;
+                    }
+                    else if (input.shield)
+                    {
+                        action = Types.ActionType.SHIELD;
+                    }
+                }
+                if (input.lightLeft || input.strongLeft)
+                {
+                    if (input.neutral)
+                    {
+                        facing = false;
+                        action = Types.ActionType.FTILT;
+                    }
+                    else if (input.special)
+                    {
+                        facing = false;
+                        action = Types.ActionType.FSPECIAL;
+                    }
+                    else if (input.shield)
+                    {
+                        facing = false;
+                        action = Types.ActionType.SPOTDODGE;
+                    }
+                }
+            }
+            else
+            {
+                if (input.lightRight || input.strongRight)
+                {
+                    if (input.neutral)
+                    {
+                        action = Types.ActionType.FAIR;
+                    }
+                    else if (input.special)
+                    {
+                        action = Types.ActionType.AIRFSPECIAL;
+                    }
+                    else if (input.shield)
+                    {
+                        action = Types.ActionType.AIRDODGE;
+                    }
+                }
+            }
+        }
+        else
+        {
+            
+        }
+
 		// Action Debug 
 		if (input.lightLeft)
 			Debug.Log("Walking Left");
