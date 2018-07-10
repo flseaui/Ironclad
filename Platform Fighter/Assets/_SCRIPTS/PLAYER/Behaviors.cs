37827d1767d803488b5dd17fb5e94f151e666a4e
LudovicoTechnique <vincenthepic@gmail.com>
2018-07-09 20:50:31 -0700

    Tonky_Behaviors

---
+++ Platform Fighter/Assets/_SCRIPTS/PLAYER/Behaviors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Types;
+
+public class Behaviors : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ThatOneMethodThatWeReallyShouldNameButWeJustCantThinkOfAName (ActionType action) {
+        
+        switch (action) {
+            case (ActionType.WALK):
+                break;
+            case (ActionType.RUN):
+                break;
+
+        }
+
+
+    }
+}
---
+++ Platform Fighter/Assets/_SCRIPTS/PLAYER/Tonky_Behaviors.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Types;
+
+public class Tonky_Behaviors : MonoBehaviour {
+
+    double acclereation;
+    double terminal_velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ActionType CurrentAction = 0;
+        acclereation = 0;
+        terminal_velocity = 0;
+        ThatOneMethodThatWeReallyShouldNameButWeJustCantThinkOfAName(CurrentAction);
+	}
+
+    public void ThatOneMethodThatWeReallyShouldNameButWeJustCantThinkOfAName (ActionType action) {
+        
+        switch (action) {
+            case (ActionType.WALK):
+                acclereation = .25;
+                terminal_velocity = 2.5;
+                break;
+
+            case (ActionType.RUN):
+                acclereation = .25;
+                terminal_velocity = 2.5;
+                break;
+
+        }
+
+
+    }
+}
