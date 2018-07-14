5be568ef734537e98314ce591d42e0870f0e86e4
Rewind <sploing1@gmail.com>
2018-07-13 22:25:50 -0700

    Started on simple character select screen
    added token and token script

---
+++ Platform Fighter/Assets/_SCRIPTS/MENU/TokenMovement.cs
@@ -0,0 +1,51 @@
+﻿using MISC;
+using Rewired;
+using UnityEngine;
+
+namespace MENU
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class TokenMovement : MonoBehaviour
+    {
+        private bool _select, _back;
+
+        private RectTransform _transform;
+
+        private Player _player;
+
+        [SerializeField]
+        private int _speed = 1000;
+
+        private void Start()
+        {
+            _player = ReInput.players.GetPlayer(0);
+            _transform = GetComponent<RectTransform>();
+        }
+
+        private void Update()
+        {
+            UpdateInput();
+        }
+
+        private void UpdateInput()
+        {
+            _transform.anchoredPosition = Vector2.Lerp
+            (
+                _transform.anchoredPosition,
+                new Vector2
+                (
+                    _transform.anchoredPosition.x + _player.GetAxis("Horizontal") * _speed * Time.deltaTime,
+                    _transform.anchoredPosition.y + _player.GetAxis("Vertical") * _speed * Time.deltaTime
+                ),
+                _speed * Time.deltaTime
+            );
+
+
+            if (_player.GetButtonDown("Select"))
+                _select = true;
+
+            if (_player.GetButtonDown("Back"))
+                _back = true;
+        }
+    }
+}
