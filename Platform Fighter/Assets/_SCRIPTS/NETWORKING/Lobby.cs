20355288f1d6847dd825cda0a3f631b0423a11af
Rewind <sploing1@gmail.com>
2018-07-15 09:10:33 -0700

    Started on lobby system

--- Platform Fighter/Assets/_SCRIPTS/NETWORKING/Lobby.cs
+++ Platform Fighter/Assets/_SCRIPTS/NETWORKING/Lobby.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+﻿using Facepunch.Steamworks;
+using UnityEngine;
 
 namespace NETWORKING
 {
     public class Lobby : MonoBehaviour
     {
-        
+        public void CreateLobby()
+        {
+            using (var client = new Client(appId: 408))
+            {
+                client.Lobby.Create(Facepunch.Steamworks.Lobby.Type.Public, 2);
+
+                client.Lobby.OnLobbyCreated = (success) =>
+                {
+                    Debug.Log("lobby created: " + client.Lobby.CurrentLobby);
+                    Debug.Log($"Owner: {client.Lobby.Owner}");
+                    Debug.Log($"Max Members: {client.Lobby.MaxMembers}");
+                    Debug.Log($"Num Members: {client.Lobby.NumMembers}");
+
+                    client.Lobby.Leave();
+
+                };
+            }
+        }
     }
 }
