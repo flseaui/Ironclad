e8d526b2ed807469f327d427a2d620f9680efe32
Rewind <sploing1@gmail.com>
2018-07-15 20:24:53 -0700

    Added find and join lobby buttons as well as partially functional lobby list

--- Platform Fighter/Assets/_SCRIPTS/NETWORKING/Lobby.cs
+++ Platform Fighter/Assets/_SCRIPTS/NETWORKING/Lobby.cs
@@ -5,23 +5,46 @@ namespace NETWORKING
 {
     public class Lobby : MonoBehaviour
     {
+        [SerializeField] private GameObject _serverList, _serverButtonPrefab;
+
         public void CreateLobby()
         {
-            using (var client = new Client(appId: 408))
-            {
-                client.Lobby.Create(Facepunch.Steamworks.Lobby.Type.Public, 2);
+            Client.Instance.Lobby.Create(Facepunch.Steamworks.Lobby.Type.Public, 2);
 
-                client.Lobby.OnLobbyCreated = (success) =>
+            Client.Instance.Lobby.OnLobbyCreated = delegate
                 {
-                    Debug.Log("lobby created: " + client.Lobby.CurrentLobby);
-                    Debug.Log($"Owner: {client.Lobby.Owner}");
-                    Debug.Log($"Max Members: {client.Lobby.MaxMembers}");
-                    Debug.Log($"Num Members: {client.Lobby.NumMembers}");
-
-                    client.Lobby.Leave();
-
+                    Debug.Log("lobby created: " + Client.Instance.Lobby.CurrentLobby);
+                    Debug.Log($"Owner: {Client.Instance.Lobby.Owner}");
+                    Debug.Log($"Max Members: {Client.Instance.Lobby.MaxMembers}");
+                    Debug.Log($"Num Members: {Client.Instance.Lobby.NumMembers}");
                 };
-            }
+        }
+
+        public void JoinLobby(ulong id)
+        {
+            
+            Client.Instance.Lobby.Join(Client.Instance.Lobby.CurrentLobby);
+        }
+
+        public void FindLobbies()
+        {
+            Client.Instance.LobbyList.Refresh();
+            
+            //wait for the callback
+            foreach (Transform child in _serverList.transform)
+                Destroy(child.gameObject);
+
+            Client.Instance.LobbyList.OnLobbiesUpdated = delegate
+            {
+                if (!Client.Instance.LobbyList.Finished) return;
+
+                foreach (var lobby in Client.Instance.LobbyList.Lobbies)
+                {
+                    Instantiate(_serverButtonPrefab, _serverList.transform);
+                    Debug.Log($"Found Lobby: {lobby.Name}");
+                }
+            };
+
         }
     }
 }
