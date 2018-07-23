e74571f95079258fd4e4cfd9e60330556a972bfe
Rewind <sploing1@gmail.com>
2018-07-23 09:24:45 -0700

    Added joinLobby click listener to each lobby button

--- Platform Fighter/Assets/_SCRIPTS/MENU/LobbySelectMenu.cs
+++ Platform Fighter/Assets/_SCRIPTS/MENU/LobbySelectMenu.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using ATTRIBUTES;
 using Facepunch.Steamworks;
+using JetBrains.Annotations;
 using MANAGERS;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Experimental.UIElements;
 using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
 using Types = DATA.Types;
 
 namespace MENU
@@ -51,11 +54,19 @@ namespace MENU
                     var button = Instantiate(_serverButtonPrefab, _serverList.transform);
                     button.GetComponentInChildren<Text>().text = lobby.Name;
                     button.name = lobby.Name;
+                    button.GetComponent<Button>().onClick.AddListener(delegate { JoinLobby(lobby.LobbyID); });
                     Debug.Log($"Found Lobby: {lobby.Name}");
                 }
            };
-        }
 
+            void JoinLobby(ulong lobbyId)
+            {
+                Client.Instance.Lobby.Join(lobbyId);
+                Debug.Log($"Joined Lobby: {Client.Instance.Lobby.Name}");
+            }
+            
+        }
+        
         public void SwitchToMultiplayerMenu() => MenuManager.Instance.MenuState = Types.Menu.MultiplayerMenu;
     }
 }
